b61b6288d2f37130362e6fb2ae52373c255d39b6
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-04-05 05:32:58 +0000

    Complete Assignment???

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,56 @@
+import java.util.Arrays;
+import java.util.ArrayList;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(Arrays.toString(recaman(9)));
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+
+public static int[] recaman(int n) {
+	if (n<0){
+		return new int[]{};
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+int[] array = new int[n];
+ArrayList<Integer> arr = new ArrayList<Integer>();
+recamanMaker(n, arr, array);
+return array;
+}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+public static void recamanMaker(int n, ArrayList<Integer> arr, int[] array){
 
-	public static void q5() {
-		//Write question 5 code here
+
+System.out.println(recHelper(n, arr));
+
+for (int i = 0; i<array.length; i++){
+	array[i] = arr.get(i);
+}
+}
+
+
+public static int recHelper(int n, ArrayList<Integer> list){
+if (n ==0){
+	return 0;
+}
+int temp = recHelper(n-1, list)-n;
+System.out.println(n + " this is n current");
+System.out.println(temp + " this is temp current");
+if (temp>0&&!list.contains(temp)){
+	System.out.println(n + " this is n");
+	System.out.println(temp + " this is temp sub");
+	list.add(temp);
+}
+else{
+	temp = temp+2*n;
+	if (!list.contains(temp)){
+		System.out.println(n + " this is n");
+		System.out.println(temp + " this is temp add");
+		list.add(temp);
 	}
+}
+return temp;
+}
 
 }
